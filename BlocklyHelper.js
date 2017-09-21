35415f8af98c16d86c8ed26600bbb79972c8e9e2
James Collins <james.collins@outlook.com.au>
2017-09-22 08:58:59 +1000

    Updated to version 0.8
    Blockly Helper is now an object instead of seperate functions

--- BlocklyHelper.js
+++ BlocklyHelper.js
@@ -1,107 +1,123 @@
 /**
-*	File: BlocklyHelper.js
-**/
-var bh_toolboxXml = '';
-var bh_toolboxXmlCurrent = '';
+ * @file Blockly Helper class
+ * @author James Collins
+ * @version 0.8
+ *
+ */
 
+function BlocklyHelper(workspace, toolboxXml) {
+	this._workspace = workspace;
+	this._toolboxXml = toolboxXml;
 
-//--
-// XML ForEachByTagName
-//--
-function xmlForEachByTagName(xml, tagName, func) {
-	nodes = xml.getElementsByTagName(tagName);
-	for(i = 0; i < nodes.length; i++) {
-		if(func(nodes[i]) == false) {
-			return;
-		}
-	}
-}
-
-
-//--
-// Load Toolbox XML from document by Element ID
-//--
-function BlocklyHelper_init(workspace, toolboxId, mode) {
-	bh_toolboxXml = document.getElementById(toolboxId);
-	bh_toolboxXmlCurrent = bh_toolboxXml.cloneNode(true);
-
-	workspace.addChangeListener(BlocklyHelper_Listener);
-
-	if(mode != '') {
-		BlocklyHelper_ToolboxMode(workspace, mode);
-	}
-
-	workspace.updateToolbox(bh_toolboxXmlCurrent);
-}
-
-
-//--
-// Change the Toolbox to a specific mode
-//--
-function BlocklyHelper_ToolboxMode(workspace, mode) {
-	bh_toolboxXmlCurrent = bh_toolboxXml.cloneNode(true);
+/**
+  * Change the toolbox mode
+  * @param {string} Show categories/blocks that are blank or set to mode
+  */
+  	this.toolboxMode = (function(mode) {
+		var updated = false;
 	
-	f = function(n) {
-		modeVal = n.getAttribute('mode');
-		if(modeVal != null && modeVal != '' && modeVal != mode) {
-			n.parentNode.removeChild(n);
-		}
-	};
-	
-	xmlForEachByTagName(bh_toolboxXmlCurrent, 'category', f);
-	xmlForEachByTagName(bh_toolboxXmlCurrent, 'block', f);
-
-	workspace.updateToolbox(bh_toolboxXmlCurrent);
-}
-
-
-//--
-// Disable Block by type
-//--
-function BlocklyHelper_DisableBlock(workspace, type, disable) {
-	f = function(node) {
-		if(node.getAttribute('type') == type) {
-			node.setAttribute('disabled', disable);
-		}
-	}
-
-	xmlForEachByTagName(bh_toolboxXml, 'block', f);
-	xmlForEachByTagName(bh_toolboxXmlCurrent, 'block', f);
-	
-	workspace.updateToolbox(bh_toolboxXmlCurrent);
-}
-
-
-//--
-// Blockly Listener callback to update toolbox
-//--
-function BlocklyHelper_Listener(event) {
-	found = false;
-	disable = true;
-	xml = null;
-
-	f = function(n) {
-		if(n.getAttribute('type') == xml.attributes.getNamedItem('type').value) {
-			if(n.getAttribute('singleton') == 'true') {
-				n.setAttribute('disabled', disable);
-				found = true;
-			} else {
-				return false;
+		var nodes = this._toolboxXml.getElementsByTagName("*");
+		for(var i = 0; i < nodes.length; i++) {
+			if((nodes[i].tagName == "CATEGORY" || nodes[i].tagName == "BLOCK") && (nodes[i].hasAttribute("mode"))) {
+			 	if(nodes[i].getAttribute("mode") != mode) {
+			 		if(nodes[i].getAttribute("hidden") != "true") {
+			 			nodes[i].setAttribute("hidden", "true");
+			 			updated = true;
+			 		}
+			 	} else {
+			 		if(nodes[i].getAttribute("hidden") == "true") {
+			 			nodes[i].setAttribute("hidden", "false");
+			 			updated = true;
+			 		}
+			 	}
 			}
 		}
-	}
+		
+		if(updated == true) {
+			this.toolboxUpdate();
+		}
+	}).bind(this);
+	
 
-	if(event.type == Blockly.Events.BLOCK_CREATE) {
-		disable = true;
-		xml = event.xml;
+/**
+  * Disable/Enable a block in the toolbox
+  * @param {string} Block type to disable/enable
+  * @param {boolean} Disable or enable the block
+  */
+	this.blockDisable = (function(type, disabled) {
+		var updated = false;
+	
+		var nodes = this._toolboxXml.getElementsByTagName("block");
+		for(var i = 0; i < nodes.length; i++) {
+			if(nodes[i].getAttribute("type") == type) {
+				if(nodes[i].getAttribute("disabled") != disabled) {
+					nodes[i].setAttribute("disabled", disabled);
+					update = true;
+				}
+			}
+		}
+
+		if(updated == true) {
+			this.toolboxUpdate();
+		}
+	}).bind(this);
+
+
+/**
+  * Update the blockly toolbox
+  */
+	this.toolboxUpdate = (function() {
+		var xml = this._toolboxXml.cloneNode(true);
+
+		var nodes = xml.getElementsByTagName("category");
+		for(var i = 0; i < nodes.length; i++) {
+			if(nodes[i].getAttribute("hidden") == "true") {
+				nodes[i].parentNode.removeChild(nodes[i]);
+			}
+		}
+
+		var nodes = xml.getElementsByTagName("block");
+		for(var i = 0; i < nodes.length; i++) {
+			if(nodes[i].getAttribute("hidden") == "true") {
+				nodes[i].parentNode.removeChild(nodes[i]);
+			}
+		}
 		
-		xmlForEachByTagName(bh_toolboxXmlCurrent, 'block', f);
-		if(found) xmlForEachByTagName(bh_toolboxXml, 'block', f);
-	} else if(event.type == Blockly.Events.BLOCK_DELETE) {
-		disable = false;
-		xml = event.oldXml;
+		this._workspace.updateToolbox(xml);
+	}).bind(this);
+	
+
+/**
+  * (Private) Blockly workspace change listener
+  * @param {object} event data passed from Google Blockly workspace
+  */
+	this._changeListener = (function(e) {
+		var updated = false;
+
+		if(e.type == Blockly.Events.BLOCK_CREATE) {
+			var nodes = this._toolboxXml.getElementsByTagName("block");
+			for(var i = 0; i < nodes.length; i++) {
+				if(nodes[i].getAttribute("type") == e.xml.attributes.getNamedItem("type").value && nodes[i].getAttribute("singleton") == "true") {
+					nodes[i].setAttribute("disabled", "true");
+					updated = true;
+				}
+			}
+		} else if(e.type == Blockly.Events.BLOCK_DELETE) {
+			var nodes = this._toolboxXml.getElementsByTagName("block");
+			for(var i = 0; i < nodes.length; i++) {
+				if(nodes[i].getAttribute("type") == e.oldXml.attributes.getNamedItem("type").value && nodes[i].getAttribute("singleton") == "true") {
+					nodes[i].setAttribute("disabled", "false");
+					updated = true;
+				}
+			}
+		}
 		
-		xmlForEachByTagName(bh_toolboxXmlCurrent, 'block', f);
-		if(found) xmlForEachByTagName(bh_toolboxXml, 'block', f);
-	}
+		if(updated == true) {
+			this.toolboxUpdate();
+		}
+	}).bind(this);
+
+/** rest of initialisation code **/
+	this._workspace.addChangeListener(this._changeListener);
+	this.toolboxUpdate();
 }
